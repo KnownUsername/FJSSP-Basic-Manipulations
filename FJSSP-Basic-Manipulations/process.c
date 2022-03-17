a635585a8e1762250304977570e132ac89188e88
KnownUsername <joaoftrodrigues@gmail.com>
2022-03-17 18:50:37 +0000

    Started processes development (Create, Insert, Check if element exists on list)

---
+++ FJSSP-Basic-Manipulations/process.c
@@ -0,0 +1,109 @@
+#include "process.h"
+#include <stdlib.h>
+
+/// <summary>
+/// Creates a Process with given values
+/// </summary>
+/// <param name="machine"></param>
+/// <param name="time"></param>
+/// <returns></returns>
+Process CreateProcess(int machine, int time) {
+	Process createdElement;
+
+	// Values' attribution
+	createdElement.machine = machine;
+	createdElement.time = time;
+
+	return createdElement;
+}
+
+/// <summary>
+/// Creates a ProcessList 'element' with a Process
+/// </summary>
+/// <param name="process"></param>
+/// <returns></returns>
+ProcessList CreateProcessListElement(Process process) {
+	ProcessList createdProcessList;
+
+	// Values' attribution
+	createdProcessList.process = process;
+	createdProcessList.nextProcess = NULL;
+
+	return createdProcessList;
+}
+
+/// <summary>
+/// Verify if process exists on a list
+/// </summary>
+/// <param name="processList"></param>
+/// <param name="machine"></param>
+/// <returns> 1 -> True | 0 -> False </returns>
+int ProcessExists(ProcessList* processList, int machine) {
+
+	// Run through process list
+	while (processList) {
+
+		// Compare machines' values
+		if (processList->process.machine == machine) return 1; // True
+
+		// Navigate to next process
+		processList = processList->nextProcess;
+
+	}
+
+	return 0; // False
+
+}
+
+/// <summary>
+/// Inserts a Process on a list of processes
+/// </summary>
+/// <param name="processList"></param>
+/// <param name="machine"></param>
+/// <param name="time"></param>
+/// <returns></returns>
+ProcessList* InsertProcess(ProcessList* processList, Process newProcess) {
+	
+	// Empty List
+	if (!processList) {
+
+		// Struture to store new element on list
+		//ProcessList aux = CreateProcessListElement(newProcess);
+		
+		// Unique element is the pointer to new element
+		processList = (ProcessList*)malloc(sizeof(ProcessList));
+		processList->process = newProcess;
+		processList->nextProcess = NULL;
+
+	}
+
+	// List with elements
+	else {
+
+		// Check if process is already on operation
+		if (!ProcessExists(processList, newProcess.machine)) {
+			
+			ProcessList* lastElement = processList;
+
+			// Search last element on list
+			while (lastElement->nextProcess) {
+				lastElement = lastElement->nextProcess;
+			}
+
+			// Struture to store new element on list
+			ProcessList aux = CreateProcessListElement(newProcess);
+			
+			// Connect last element to new one, by pointers
+			//lastElement->nextProcess = &aux;
+			
+			lastElement->nextProcess = (ProcessList*)malloc(sizeof(ProcessList));
+			lastElement->nextProcess->process = newProcess;
+			lastElement->nextProcess->nextProcess = NULL;
+
+		}
+	}
+
+	// Pointer to 1st element
+	return processList;
+}
+
