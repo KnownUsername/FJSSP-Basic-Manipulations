63842581b612525a3fb2a2e5582b1155b8e88d89
KnownUsername <joaoftrodrigues@gmail.com>
2022-03-19 19:29:06 +0000

    Updated to test removals (Processes and Operations)

--- FJSSP-Basic-Manipulations/main.c
+++ FJSSP-Basic-Manipulations/main.c
@@ -36,21 +36,41 @@ int main() {
 	}
 	else printf("Unsucessfull");
 
-	processList = RemoveProcess(processList, 1);
+	processList = RemoveProcess(processList, 6);
 
-	ShowProcessList(processList);
+	//ShowProcessList(processList);
 
 	Operation operation;
-	operation = CreateOperation(1, *processList);
+	operation = CreateOperation(1, processList);
 
-	Operation operation2 = CreateOperation(2, *processList);
+	/*			Operation 2			*/	
+	ProcessList* processList2 = NULL;
+	processList2 = InsertProcess(processList2, CreateProcess(1, 5));
+	InsertProcess(processList2, CreateProcess(3, 7));
+	InsertProcess(processList2, CreateProcess(4, 2));
 
+	Operation operation2 = CreateOperation(2, processList2);
 
+	/*			Operation 3			*/
+	ProcessList* processList3 = NULL;
+	processList3 = InsertProcess(processList3, CreateProcess(2, 2));
+	InsertProcess(processList3, CreateProcess(4, 3));
+	InsertProcess(processList3, CreateProcess(5, 7));
+	InsertProcess(processList3, CreateProcess(6, 8));
+
+	Operation operation3 = CreateOperation(3, processList3);
+
+	/*		List of Operations		*/
 	OperationList* operations = NULL;
 	operations = InsertOperation(operations, operation);
 	InsertOperation(operations, operation2);
+	InsertOperation(operations, operation3);
 
-	//ShowOperation(operation);
+
+
+	// Removal of an operation
+	operations = RemoveOperation(operations, 3);
+
 	ShowOperationList(operations);
 	
 	return 0;
